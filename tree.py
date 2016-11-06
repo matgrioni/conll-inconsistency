7f6a1a192772e0fcb4d11c206bd116cf902b1bd9
Matias Grioni <matgrioni@gmail.com>
2016-11-06 12:41:06 -0500

    Included helper methods for checking if node value in tree, and for getting all subtrees rooted at a certain value

--- tree.py
+++ tree.py
@@ -9,6 +9,34 @@ class Tree(object):
             child.parent = self
             self.children.append(child)
 
+    # Returns the subtrees whose root nodes equal the provided node.
+    # If there are no such trees than an empty list is returned.
+    def find_trees_by_node(self, node):
+        trees = []
+
+        if self.node == node:
+            trees.append(self)
+        else:
+            for child in children:
+                child_matches = child.find_trees_by_node(node)
+                if child_matches:
+                    trees += child_matches
+
+        return tree
+
+    # Checks if the given node is in the tree and returns a boolean
+    # response. To be used with `in` operator.
+    def __contains__(self, value):
+        contains = self.node == value
+        if not contains:
+            for child in self.children:
+                contains = value in child
+
+                if contains:
+                    break
+
+        return contains
+
     def size(self):
         s = 0
 
