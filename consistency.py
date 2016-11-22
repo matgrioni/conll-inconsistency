5fdb69c0aca91cbceef9d88496c3d1f440ef27e1
Matias Grioni <matgrioni@gmail.com>
2016-11-22 09:36:47 -0500

    Large refactor to use line numbers instead of sent_id since not all language corpus are formatted the same way

--- consistency.py
+++ consistency.py
@@ -26,53 +26,10 @@ INTERNAL_CONTEXT = ('-i', '-internal')
 LEFT = "L"
 RIGHT = "R"
 NIL = "NIL"
+NIL_RELATION = (NIL, NIL)
 
-ContextVariation = namedtuple('ContextVariation', ['internal_ctx', 'external_ctx', 'head_dep', 'index', 'sent_id'])
-Error = namedtuple('Error', ['lemmas', 'dependency1', 'dependency2', 'index1', 'index2', 'sent_id1', 'sent_id2'])
-
-# Find if there exist any NIL dependencies in the tree that match the
-# dependencies provided.
-# Finds all direct dependencies. Essentially goes through all nodes in
-# the tree and combines the root and each child into 2-tuples. The
-# tuples are composed of the lemmas of the nodes.
-def find_relations(sentence, relations):
-    nil_relation = (NIL, NIL)
-
-    for i, word1 in enumerate(sentence):
-        for word2 in sentence[i+1:]:
-            # The two words are not related
-            if word1.dep_index != word2.index and word2.dep_index != word1.index:
-                nil_lemmas = frozenset((word1.lemma, word2.lemma))
-
-                internal_ctx = calc_internal_context(sentence, word1, word2)
-                external_ctx = calc_external_context(sentence, word1, word2)
-
-                first_index = min(word1.index, word2.index)
-                context = ContextVariation(internal_ctx, external_ctx, NIL, first_index, sentence.id)
-
-                relations[nil_lemmas][nil_relation].append(context)
-            else:
-                if word1.dep_index == word2.index:
-                    head = word2
-                    child = word1
-                elif word2.dep_index == word1.index:
-                    head = word1
-                    child = word2
-                related_lemmas = frozenset((head.lemma, child.lemma))
-
-                internal_ctx = calc_internal_context(sentence, head, child)
-
-                direction = LEFT if head.index < child.index else RIGHT
-                if direction == LEFT:
-                    external_ctx = calc_external_context(sentence, head, child)
-                else:
-                    external_ctx = calc_external_context(sentence, child, head)
-
-                first_index = min(head.index, child.index)
-                context = ContextVariation(internal_ctx, external_ctx, head.dep, first_index, sentence.id)
-
-                # TODO: Comment this or actually make it readable
-                relations[related_lemmas][(direction, child.dep)].append(context)
+ContextVariation = namedtuple('ContextVariation', ['internal_ctx', 'external_ctx', 'head_dep', 'line_number'])
+Error = namedtuple('Error', ['dependency1', 'dependency2', 'line_number1', 'line_number2'])
 
 # Get the external context of the two words in the given sentence as a
 # binary tuple of lemmas. The two words should be in the sentence and
@@ -97,6 +54,17 @@ def calc_internal_context(sentence, word1, word2):
     words = sentence[word1.index : word2.index - 1] or sentence[word2.index : word1.index - 1]
     return map(lambda word: word.lemma, words)
 
+def print_errors(errors, header):
+    print '----------------------  {}  ----------------------'.format(header)
+    for lemmas, lemma_errors in errors.items():
+        print ', '.join(lemmas)
+        for error in lemma_errors:
+            dep1, dep2 = ', '.join(error.dependency1), ', '.join(error.dependency2)
+            print '\t{} at {}'.format(dep1, error.line_number1)
+            print '\t{} at {}'.format(dep2, error.line_number2)
+
+            print
+
 ######################################################################
 #
 # Main script
@@ -109,19 +77,64 @@ if len(sys.argv) < 2:
 dep_heuristic = reduce(lambda acc, option: acc or option in sys.argv, DEPENDENCY_CONTEXT, False)
 internal_ctx_pres = reduce(lambda acc, option: acc or option in sys.argv, INTERNAL_CONTEXT, False)
 
-filename = sys.argv[1]
-with open(filename) as f:
-    treebank = TreeBank(f.read())
-
 # Find all relations in the treebank and then consolidate the ones
 # with identical types but different labels.
 # TODO: Explain why defaultdict
+filename = sys.argv[1]
 relations = defaultdict(lambda: defaultdict(list))
-for sentence in treebank:
-    find_relations(sentence, relations)
+with open(filename) as f:
+    lines = []
+    sent_start = -1
+    for l, line in enumerate(f):
+        if line:
+            if line[0] != "#":
+                if sent_start < 0:
+                    sent_start = l
+                lines.append(line)
+        else:
+            sentence = Sentence("\n".join(lines))
+            for i, word1 in enumerate(sentence):
+                for word2 in lines[i+1:]:
+                    # The two words are not related
+                    if word1.dep_index != word2.index and word2.dep_index != word1.index:
+                        nil_lemmas = frozenset((word1.lemma, word2.lemma))
 
-nil_errors = []
-context_errors = []
+                        internal_ctx = calc_internal_context(sentence, word1, word2)
+                        external_ctx = calc_external_context(sentence, word1, word2)
+
+                        first_index = min(word1.index, word2.index)
+                        context = ContextVariation(internal_ctx, external_ctx, NIL, sent_start +  word1.index - 1)
+
+                        relations[nil_lemmas][NIL_RELATION].append(context)
+                    else:
+                        if word1.dep_index == word2.index:
+                            head = word2
+                            child = word1
+                        elif word2.dep_index == word1.index:
+                            head = word1
+                            child = word2
+                        related_lemmas = frozenset((head.lemma, child.lemma))
+
+                        internal_ctx = calc_internal_context(sentence, head, child)
+
+                        if (internal_ctx_present and internal_ctx) or not internal_ctx_present:
+                            direction = LEFT if head.index < child.index else RIGHT
+                            if direction == LEFT:
+                                external_ctx = calc_external_context(sentence, head, child)
+                            else:
+                                external_ctx = calc_external_context(sentence, child, head)
+
+                            first_index = min(head.index, child.index)
+                            context = ContextVariation(internal_ctx, external_ctx, head.dep, sent_start + word1.index - 1)
+
+                            # TODO: Comment this or actually make it readable
+                            relations[related_lemmas][(direction, child.dep)].append(context)
+
+            sent_start = -1
+            del line[:]
+
+nil_errors = defaultdict(list)
+context_errors = defaultdict(list)
 
 for related_lemmas, lemma_variations in relations.items():
     # First check for NIL errors. This is where for a pair of lemmas
@@ -133,14 +146,7 @@ for related_lemmas, lemma_variations in relations.items():
             if dep != (NIL, NIL):
                 for variation in variations:
                     if variation.internal_ctx == nil_variation.internal_ctx:
-                        # If the internal context needs to be present
-                        # then check if it is before adding a new error.
-                        # Otherwise, simply add the NIL error to the list.
-                        if internal_ctx_pres:
-                            if variation.internal_ctx:
-                                nil_errors.append(Error(related_lemmas, (NIL, NIL), dep, nil_variation.index, variation.index, nil_variation.sent_id, variation.sent_id))
-                        else:
-                            nil_errors.append(Error(related_lemmas, (NIL, NIL), dep, nil_variation.index, variation.index, nil_variation.sent_id, variation.sent_id))
+                        nil_errors[related_lemmas].append(Error((NIL, NIL), dep, variation.line_number, nil_variation.line_number))
 
     # Then check for errors using the non-fringe heuristic. This
     # checks between non-NIL relations. If the external contexts
@@ -156,26 +162,16 @@ for related_lemmas, lemma_variations in relations.items():
                             # Lastly, is the check for head dependencies which is on top of the external context.
                             if dep_heuristic:
                                 if variation1.head_dep == variation2.head_dep:
-                                    context_errors.append(Error(related_lemmas, dep1, dep2, variation1.index, variation2.index, variation1.sent_id, variation2.sent_id))
+                                    context_errors[related_lemmas].append(Error(dep1, dep2, variation1.line_number, variation2.line_number))
                             else:
-                                context_errors.append(Error(related_lemmas, dep1, dep2, variation1.index, variation2.index, variation1.sent_id, variation2.sent_id))
+                                context_errors[related_lemmas].append(Error(dep1, dep2, variation1.line_number, variation2.line_number))
 
 # Print out the error results
-print '----------------------  NIL Errors  ----------------------'
-for error in nil_errors:
-    print ', '.join(error.lemmas)
-    dep1, dep2 = ', '.join(error.dependency1), ', '.join(error.dependency2)
-    print '\t{} in {}, {}#{}'.format(dep1, error.sent_id1[0], error.sent_id1[1], error.index1)
-    print '\t{} in {}, {}#{}'.format(dep2, error.sent_id2[0], error.sent_id2[1], error.index2)
+print_errors(nil_errors, 'NIL Errors')
 
 print
-    
-print '----------------------    Context Errors    ----------------------'
-for error in context_errors:
-    print ', '.join(error.lemmas)
-    dep1, dep2 = ', '.join(error.dependency1), ', '.join(error.dependency2)
-    print '\t{} in {}, {}#{}'.format(dep1, error.sent_id1[0], error.sent_id1[1], error.index1)
-    print '\t{} in {}, {}#{}'.format(dep2, error.sent_id2[0], error.sent_id2[1], error.index2)
+
+print_errors(context_errors, 'Context Errors')
 
 print
 
