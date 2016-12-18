f4df12dd854018ddb7d0b4954256346db662dee7
Matias Grioni <matgrioni@gmail.com>
2016-12-18 14:47:48 -0500

    Better documentation on annotation formatting and included unsure category

--- analyze.py
+++ analyze.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
 
+######################################################################
+# The following is an analysis tool for the output of the results of
+# a consistency analysis on a corpus. There are a few requirements
+# that must be met in order to ensure accurate results. There must be
+# a newline between the nuclei variation occurences, there must be a
+# tab character before the annoation, 'y', 'n', or '?', the file
+# must end in a newline to ensure the last annotation is counted.
+#
+# Examples of properly formatted annotations can be found in the
+# repo's README.
+######################################################################
+
 from collections import defaultdict
 from recordclass import recordclass
 
@@ -12,7 +24,7 @@ import sys
 # variation occurences that follow it.
 HEADER_REGEX = '^-+\s*(.+?)\s*-+$'
 
-VariationCount = recordclass('VariationCount', ['yes', 'no', 'unmarked'])
+VariationCount = recordclass('VariationCount', ['yes', 'no', 'unsure', 'unmarked'])
 
 if len(sys.argv) < 2:
     raise TypeError('Not enough arguments provided')
@@ -23,9 +35,9 @@ if len(sys.argv) < 2:
 # variation nuclei under the given header. The value for each set of
 # lemmas is a 3-namedtuple where the first element is the number of
 # actual inconsistencies, the second element is the number of
-# ambiguities, and the third number is the number of unmarked
-# occurences.
-variations = defaultdict(lambda: defaultdict(lambda: VariationCount(0, 0, 0)))
+# ambiguities, and the third number is the number of unsure marks, and
+# the last number is the number of unmarked occurences.
+variations = defaultdict(lambda: defaultdict(lambda: VariationCount(0, 0, 0, 0)))
 
 filename = sys.argv[1]
 with open(filename, 'r') as f:
@@ -43,9 +55,11 @@ with open(filename, 'r') as f:
             cur_header = match.group(1)
         elif cur_header:
             # If we are still in the error section, then check if we
-            # have a lemma found yet. Then check if we are on the line
-            # that tells us if the variation was legitimate or not.
-            if line[0] == '\t' and line[1] in ['y', 'n'] and cur_lemmas:
+            # have a lemma found yet.
+
+            # Then check if we are on the line that tells us if the
+            # variation was legitimate or not.
+            if line[0] == '\t' and line[1] in ['y', 'n', '?'] and cur_lemmas:
                 # Check to make sure that one variation isn't counted
                 # twice.
                 if new_variation:
@@ -53,6 +67,8 @@ with open(filename, 'r') as f:
                         variations[cur_header][cur_lemmas].yes += 1
                     elif line[1] == 'n':
                         variations[cur_header][cur_lemmas].no += 1
+                    elif line[1] == '?':
+                        variations[cur_header][cur_lemmas].unsure += 1
 
                 new_variation = False
             # Otherwise if we have current lemmas, and are on a newline
@@ -76,7 +92,7 @@ with open(filename, 'r') as f:
 
                 cur_lemmas = (first_lemma, second_lemma)
             # Otherwise, if the line starts with a tab, but does not
-            # start with a 'y' or 'n' then this is a new variation
+            # start with a 'y', 'n', '?', then this is a new variation
             # occurence.
             elif line[0] == '\t':
                 new_variation = True
@@ -87,7 +103,7 @@ for header, lemmas in variations.items():
 
     for lemma, counts in lemmas.items():
         print('{}, {}'.format(*lemma))
-        print('\tyes = {}, no = {}, unmarked = {}'.format(*counts))
+        print('\tyes = {}, no = {}, unsure = {}, unmarked = {}'.format(*counts))
 
         print
 
