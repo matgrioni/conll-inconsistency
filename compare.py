97f84d7863dddd9b41f9c2423304c24b31c150fb
Matias Grioni <matgrioni@gmail.com>
2017-01-16 11:35:44 -0500

    Separated transfer and compare functionality into different scripts

---
+++ compare.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+################################################################################
+# A utility script in the UD consistency workflow. This script will compare two
+# different outputs from the consistency.py script. The first output file should
+# be annotated using the guidelines explained in README. It does not matter if
+# the second file is annotated or not. The results will be as follows:
+#
+#   - The number of total occurences in the first file.
+#   - The number of annotated occurences in the first file.
+#   - The number of occurences in the first file also in the second.
+#   - The number of annotated occurences in the first file also in the second.
+#   - The number of annotated occurences that were only in the first file, that
+#     were are also correct in the original corpus (i.e. has 'y' annotation).
+#
+#   Example Usage:
+#       ./compare.py errors.txt errors-dep.txt
+#
+# The idea behind this script is to be able to quantify different heuristics
+# and techniques. The results of this script will reveal the relative precision
+# and recall for different heuristics.
+#
+# NOTE: That the comparison right now only works when the occurences in the
+#       second output file are a subset of the occurences in the first input
+#       file.
+################################################################################
+
+import sys
+
+from lib.annotation import Annotation
+
+if len(sys.argv) < 3:
+    raise TypeError("Not enough arguments provided")
+
+first = Annotation()
+first.from_filename(sys.argv[1])
+
+second = Annotation()
+second.from_filename(sys.argv[2])
+
+# The counts to be displayed as results. total and annotated are 2-tuples where
+# the first item represents the amount from the first file, and the second item
+# represents the amount that are shared between the first and second file.
+total = [0, 0]
+annotated = [0, 0]
+left_out = 0
+
+for lemmas, occurrences in first.annotations.items():
+    for occ in occurrences:
+        total[0] += 1
+
+        shared = second.has_line(lemmas, occ)
+        if shared:
+            total[1] += 1
+
+        if occ.is_annotated():
+            annotated[0] += 1
+            if shared:
+                annotated[1] += 1
+            else:
+                if occ.correct_in_corpus():
+                    left_out += 1
+
+print '{} / {} of occurrences are annotated in {}'.format(annotated[0], total[0], sys.argv[1])
+print '{} / {} of occurences in {} are in also {}'.format(total[1], total[0], sys.argv[1], sys.argv[2])
+print '{} / {} of annotated occurences in {} are also in {}'.format(annotated[1], annotated[0], sys.argv[1], sys.argv[2])
+print '{} / {} of annotated occurences in {} NOT in {} are correct in the original corpus'.format(left_out, annotated[0] - annotated[1], sys.argv[1], sys.argv[2])
--- transfer.py
+++ transfer.py
@@ -4,36 +4,17 @@ import sys
 
 from lib.annotation import Annotation, AnnotationLine
 
-# Transfers the given annotations in the source to the destination
-# annotations. The assumption is that the source is the annotated
-# output of the consistency script without head heuristic while the
-# destination annotation is the blank consistency output of the script
-# with the head heuristic. Returns a 2-tuple where the first element
-# is the number of annotations that could not be transfered to the
-# destination, while the second number is the number of annotations
-# that were not transferred that were ambigiuous.
-
-# TODO: Update docs, and account for the fact that nil errors should
-# not be counted in the transfer. Maybe a separate compare and
-# transfer function.
-def transfer_downstream(source_ann, dest_ann):
-    not_transferred = 0
-    transferred = 0
-    amb = 0
-
-    for lemmas, occurences in source_ann.annotations.items():
-        for o in occurences:
-            if o.ann is not None:
-                if dest_ann.has_line(o.type, lemmas, o.dep, o.line_num):
-                    dest_ann.set_line(o.type, lemmas, o.dep, o.line_num, o.ann)
-                    transferred += 1
-                else:
-                    not_transferred += 1
-
-                    if o.ann == 'y':
-                        amb += 1
-
-    return (transferred, not_transferred, amb)
+################################################################################
+# A simply utility to transfer annotations that have been done in one file to
+# another file. This would be used for example in a test of stringency. There
+# are two outputs, one that used more stringent heuristics and one that used
+# less stringent heuristics. The less stringent output is annotated and then
+# this script is used to transfer over the annotations. Only annotations which
+# are present in both files will be transferred over.
+#
+#   Example Usage:
+#       ./transfer.py errors.txt errors-dep.txt
+################################################################################
 
 if len(sys.argv) < 3:
     raise TypeError("Not enough arguments provided")
@@ -49,6 +30,7 @@ dest_ann.from_filename(dest_filename)
 results = transfer_downstream(source_ann, dest_ann)
 dest_ann.output(dest_filename)
 
-print '{} / {} annotations were transferred'.format(results[0], results[0] + results[1])
-print '{} / {} annotations that were not transferred were also correctly annotated in the original corpus'.format(results[2], results[1])
-print 'The closer to 1 this ratio is, the better the head heuristic because it does not include cases that are not inconsistent that are in the less stringent results. The closer to 0 this ratio is, the worse it is because it removes actual inconsistencies from the results'
+for lemmas, occurences in source_ann.annotations.items():
+    for o in occurences:
+        if o.is_annotated():
+            dest_ann.set_line(lemmas, o, o.ann)
