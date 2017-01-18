0c55d9253d4b48dcf9dd29c488a3c7dcbe49d6b5
Matias Grioni <matgrioni@gmail.com>
2017-01-17 22:53:58 -0500

    Simplified analyze script output and switched to Annotation library

--- analyze.py
+++ analyze.py
@@ -19,81 +19,42 @@ from recordclass import recordclass
 import re
 import sys
 
+from lib.annotation import Annotation
+
 VariationCount = recordclass('VariationCount', ['correct', 'incorrect', 'unmarked'])
 
 if len(sys.argv) < 2:
     raise TypeError('Not enough arguments provided')
 
-# The structure of the variations dictionary is such that each key is
-# a pair of lemmas that is found in the annotated file. Each value is
-# then a 3-namedtuple where the first element is the number of actual
-# inconsistencies, the second element is the number of ambiguities,
-# and the last number is the number of unmarked occurences.
-variations = defaultdict(lambda: VariationCount(0, 0, 0))
-
 total_count = 0
 annotated_count = 0
 
 filename = sys.argv[1]
-with open(filename, 'r') as f:
-    cur_lemmas = None
+ann = Annotation()
+ann.from_filename(filename)
 
-    for line in f:
-        if line[0] == '\t':
-            total_count += 1
-            annotation = line[-2]
-
-            if annotation == 'y':
-                variations[cur_lemmas].correct += 1
-                annotated_count += 1
-            elif annotation == 'n':
-                variations[cur_lemmas].incorrect += 1
-                annotated_count += 1
-            else:
-                variations[cur_lemmas].unmarked += 1
-        elif line not in ['\n', '\r\n']:
-            # This line is starting off a pair of lemmas, so split
-            # it into the two lemmas, ignoring the '\n' in the
-            # second lemma.
-            comma = line.index(', ')
-            first_lemma = line[:comma]
-            second_lemma = line[comma + 2:-1]
-
-            cur_lemmas = (first_lemma, second_lemma)
-
-# Print the output format
-print 'This is the output format'
-print 'lemma1, lemma2'
-print '\t# inconsistent|# total|% precision'
-
-print
-
-fully_inconsistent = 0
 accounted = 0
+acc_flag = False
 inconsistent_tokens = 0
 total_tokens = 0
-for lemma_pair, count in variations.items():
-    # Do not include unfinished counts.
-    if count.unsure == 0 and count.unmarked == 0:
-        accounted += 1
-        print '{}, {}'.format(*lemma_pair)
 
-        total = count.correct + count.incorrect
-        inconsistent_tokens += count.incorrect
-        total_tokens += total
-        print '\t{}\t{}\t{}%'.format(count.incorrect, total, count.incorrect / total * 100)
+for lemma_pair, occurrences in ann.annotations.items():
+    for occ in occurrences:
+        if occ.is_annotated():
+            if not acc_flag:
+                acc_flag = True
+                accounted += 1
 
-        if count.correct == 0:
-            fully_inconsistent += 1
+            total_tokens += 1
+
+            if not occ.correct_in_corpus():
+                inconsistent_tokens += 1
 
 if accounted > 0:
-    print 'Total number of occurrences: {}'.format(total_count)
-    print 'Total number of annotated occurrences: {}'.format(annotated_count)
+    print 'Percent of all occurences that were correct'
+    print '{} / {} = {}%'.format(total_tokens - inconsistent_tokens, total_tokens, (total_tokens - inconsistent_tokens) / total_tokens)
 
-    print 'Percent of lemmas where all tokens were inconsistent'
-    print '{} / {} = {}%'.format(fully_inconsistent, accounted, fully_inconsistent / accounted * 100)
-
-    print 'Percent of all tokens that were inconsistent'
+    print 'Percent of all occurences that were incorrect'
     print '{} / {} = {}%'.format(inconsistent_tokens, total_tokens, inconsistent_tokens / total_tokens)
 else:
     print 'No instances were counted because they all had at least'
