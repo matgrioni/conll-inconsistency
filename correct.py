b059bbab70d1de8e30ff3897567787af119a1bd7
Matias Grioni <matgrioni@gmail.com>
2017-04-17 12:32:28 -0400

    Added the error checking portion.

--- correct.py
+++ correct.py
@@ -22,7 +22,9 @@ MAX_RELATION = 'max_relation'
 # lemmas can occur with. This includes the immediate neighboring words outside
 # of the pair (external context), words inside the pair (internal context), and
 # the dependency relation that the head of the pair has to its respective head.
-Context = Context('Context', ['internal_ctx', 'external_ctx', 'head_dep'])
+Context = namedtuple('Context', ['internal_ctx', 'external_ctx', 'head_dep'])
+
+Error = namedtuple('Error', ['lines', 'relationship'])
 
 # Finds the external context around these two words as a 2-tuple. The first item
 # of the tuple is the external lemma before the first word in the sentence. The
@@ -99,6 +101,8 @@ automatic_t.from_filename(sys.argv[1])
 auto_nuclei = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 
 for sentence in automatic_t:
+    # NOTE: This efficiency could be improved by traversing tree instead of
+    # going through every pair of words.
     index_pairs = itertools.combinations(range(len(sentence.words)), 2)
     for index_pair in index_pairs:
         word1 = sentence[index_pair[0]]
@@ -129,3 +133,35 @@ for sentence in automatic_t:
             if updated_value > auto_nuclei[lemmas][context][MAX_VALUE]:
                 auto_nuclei[lemmas][context][MAX_VALUE] = updated_value
                 auto_nuclei[lemmas][context][MAX_RELATION] = relationship
+
+errors = defaultdict(lambda: defaultdict(list))
+
+# NOTE: Is there any way to combine these two loops, seems awfully repetitive.
+# No common code can be put into method? Possibly a generator.
+for sentence in t:
+    # Same note about efficiency.
+    index_pairs = itertools.combinations(range(len(sentence.words)), 2)
+    for index_pair in index_pairs:
+        word1 = sentence[index_pair[0]]
+        word2 = sentence[index_pair[1]]
+
+        if word1.index == word2.dep_index or word2.index == word1.dep_index:
+            lemmas = frozenset((word1.lemma, word2.lemma))
+
+            internal = _internal_context(sentence, word1, word2)
+            external = _external_context(sentence, word1, word2)
+
+            if word1.index == word2.dep_index:
+                head = word1
+                child = word2
+            else:
+                head = word2
+                child = word1
+
+            context = Context(internal, external, head.dep)
+            direction = LEFT if sentence.indexes[head.index] < sentence.indexes[child.index] else RIGHT
+            relationship = (direction, child.dep)
+
+            if relationship != auto_nuclei[lemmas][context][MAX_RELATION]:
+                e = Error((word1.line_num, word2.line_num), relationship)
+                errors[lemmas][context].append(e)
